606b2973c76ab4dad16e5ce2b17b86635ea7f9df
barema4 <samrubarema6@gmail.com>
2019-09-09 16:16:44 +0300

    add all products

--- sport/src/components/ProductList.vue
+++ sport/src/components/ProductList.vue
@@ -3,8 +3,9 @@
         <div v-for="p in products" v-bind:Key="p.id" class="card m-1 p-1 bg-light">
             <h4>
                 {{p.name}}
-                <span class="badge badge-pill badge-primary float-right">{{p.rice}}</span>
+                <span class="badge badge-pill badge-primary float-right">{{ p.price | currency }}</span>
             </h4>
+            <div class="card-text bg-white p-1">{{ p.description }}</div>
         </div>
     </div>
 </template>
@@ -19,6 +20,11 @@ export default {
     computed: {
         ...mapState(['products'])
 
+    },
+    filters:{
+        currency(value){
+            return new Intl.NumberFormat("en-US",{style: "currency", currency: "USD"}).format(value)
+        }
     }
 }
 </script>
--- sport/src/components/Store.vue
+++ sport/src/components/Store.vue
@@ -10,8 +10,22 @@
                 <h4 class="text-white m-2">Categories</h4>
             </div>
             <div class="col-9 bg-success p-2">
-                <h4 class="text-white m-2">Products</h4>
+                <!-- <h4 class="text-white m-2">Products</h4> -->
+                <product-list />
             </div>
         </div>
     </div>
 </template>
+<script>
+import ProductList from './ProductList'
+export default {
+    data(){
+        return{
+
+        }
+    },
+    components: {
+        ProductList
+    }
+}
+</script>
