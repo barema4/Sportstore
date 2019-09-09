58f815087904652758dec859792eaefaca43c785
barema4 <samrubarema6@gmail.com>
2019-09-09 15:33:05 +0300

    Add initial setup

---
+++ sport/src/components/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div v-for="p in products" v-bind:Key="p.id" class="card m-1 p-1 bg-light">
+            <h4>
+                {{p.name}}
+                <span class="badge badge-pill badge-primary float-right">{{p.rice}}</span>
+            </h4>
+        </div>
+    </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+    data(){
+        return{
+
+        }
+    },
+    computed: {
+        ...mapState(['products'])
+
+    }
+}
+</script>
